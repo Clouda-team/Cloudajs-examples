1c7d7f9a5b022d5b7839a75880f6b27936fd68d4
ganxun <ganxun1@163.com>
2013-08-20 15:38:54 +0800

    Update userModel.js

--- Validation/app/model/userModel.js
+++ Validation/app/model/userModel.js
@@ -2,7 +2,7 @@ Model.sduser = function(exports){
     exports.config = {
         fields: [
             {name: 'name',  type: 'string', validation:'length[1,20]|unique'},
-            {name: 'age',  type: 'int', validation:'length[1,3]'},
+            {name: 'age',  type: 'int', validation:'length[1,3]|number'},
             {name: 'userid',   type: 'int'}
         ]
     };
